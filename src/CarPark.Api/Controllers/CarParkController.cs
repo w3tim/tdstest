5f548bf2862be46f5ca8c1f9c3045ab67a960e42
Tim Porter <tim@w3t.co.uk>
2026-07-13 13:18:33 +0100

    Hook up API endpoints to CarPark Service and add HTTP result mapping.

--- src/CarPark.Api/Controllers/CarParkController.cs
+++ src/CarPark.Api/Controllers/CarParkController.cs
@@ -1,27 +1,62 @@
 using CarPark.Api.Contracts;
+using CarPark.Api.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CarPark.Api.Controllers;
 
 [ApiController]
 [Route("parking")]
-public class CarParkController : ControllerBase
+public class CarParkController(ICarParkService carParkService) : ControllerBase
 {
     [HttpGet(Name = "GetParkingStatus")]
-    public ParkingStatusResponse Get()
+    public async Task<ActionResult<ParkingStatusResponse>> Get(CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var response = await carParkService.GetStatusAsync(cancellationToken);
+
+        return Ok(response);
     }
 
     [HttpPost(Name = "ParkVehicle")]
-    public ParkVehicleResponse ParkVehicle(ParkVehicleRequest request)
+    public async Task<ActionResult<ParkVehicleResponse>> ParkVehicle(ParkVehicleRequest request,
+        CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var result = await carParkService.ParkVehicleAsync(
+            request,
+            cancellationToken);
+
+        return ToActionResult(result);
     }
 
     [HttpPost("exit", Name = "ExitVehicle")]
-    public ExitVehicleResponse ExitVehicle(ExitVehicleRequest request)
+    public async Task<ActionResult<ExitVehicleResponse>> ExitVehicle(ExitVehicleRequest request,
+        CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var result = await carParkService.ExitVehicleAsync(
+            request,
+            cancellationToken);
+
+        return ToActionResult(result);
+    }
+
+    private ActionResult<T> ToActionResult<T>(CarParkResult<T> result)
+    {
+        if (result.IsSuccess) return Ok(result.Value);
+
+        var error = new
+        {
+            result.Message
+        };
+
+        return result.Failure switch
+        {
+            CarParkFailure.InvalidVehicleRegistration => BadRequest(error),
+            CarParkFailure.InvalidVehicleType => BadRequest(error),
+            CarParkFailure.VehicleAlreadyParked => Conflict(error),
+            CarParkFailure.CarParkFull => Conflict(error),
+            CarParkFailure.VehicleNotParked => NotFound(error),
+
+            _ => throw new InvalidOperationException(
+                $"Unsupported car park failure: {result.Failure}")
+        };
     }
 }
